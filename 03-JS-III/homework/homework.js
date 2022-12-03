21dfb0a4bc14266f1f145758a48d209e00d1df4a
solangeamarillo <solange.amarillo@gmail.com>
2022-12-03 16:05:32 -0300

    homework realizada JSIII

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -4,33 +4,44 @@
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
+const { default: simpleGit } = require("simple-git");
+
 function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0
+    for (var i=0 ; i <11 ; i++) {
+      suma = suma + i
+    } return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+    return array.filter((element) => element % 2 ===0)
+   
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+ return array.map((elemento) => elemento **2)  
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce ((previous,next)=> previous + next)
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
   
   
